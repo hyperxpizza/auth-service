c65e5150f7a78078086412f98730683ec1ea4159
hyperxpizza <wojtek.frackowski@gmail.com>
2022-02-01 18:20:20 +0100

    mappers

--- pkg/impl/mapper.go
+++ pkg/impl/mapper.go
@@ -1,1 +1,27 @@
 package impl
+
+import (
+	pb "github.com/hyperxpizza/auth-service/pkg/grpc"
+	"github.com/hyperxpizza/auth-service/pkg/models"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func UnMapUser(user *pb.User) models.User {
+	return models.User{
+		ID:           user.Id,
+		Username:     user.Username,
+		PasswordHash: user.PasswordHash,
+		Created:      user.Created.AsTime(),
+		Updated:      user.Updated.AsTime(),
+	}
+}
+
+func MapUser(user models.User) *pb.User {
+	return &pb.User{
+		Id:           user.ID,
+		Username:     user.Username,
+		PasswordHash: user.PasswordHash,
+		Created:      timestamppb.New(user.Created),
+		Updated:      timestamppb.New(user.Updated),
+	}
+}
