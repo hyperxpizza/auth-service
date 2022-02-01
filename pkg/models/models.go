def25f456b80f2de28646c5d1870b02d9f702dd7
hyperxpizza <wojtek.frackowski@gmail.com>
2022-02-01 17:55:39 +0100

    add user model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,11 @@
 package models
+
+import "time"
+
+type User struct {
+	ID           int64     `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"passwordHash"`
+	Created      time.Time `json:"created"`
+	Updated      time.Time `json:"updated"`
+}
