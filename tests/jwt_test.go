b1ed4098be0b8a68e292e439a57aa2ed5a544f27
hyperxpizza <wojtek.frackowski@gmail.com>
2022-03-04 21:47:20 +0100

    logout test

--- tests/jwt_test.go
+++ tests/jwt_test.go
@@ -88,3 +88,50 @@ func TestTimeValidation(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+//go test -v ./tests --run TestLogout --config=/home/hyperxpizza/dev/golang/reusable-microservices/auth-service/config.json
+func TestLogout(t *testing.T) {
+	flag.Parse()
+
+	validateFlags := func() error {
+		if *usernameOpt == "" {
+			return errors.New("username flag not set")
+		}
+
+		if *configPathOpt == "" {
+			return errors.New("config path not set")
+		}
+
+		return nil
+	}
+
+	err := validateFlags()
+	assert.NoError(t, err)
+
+	cfg, err := config.NewConfig(*configPathOpt)
+	assert.NoError(t, err)
+
+	cfg.AuthService.ExpirationTimeAccess = 2
+	cfg.AuthService.ExpirationTimeRefresh = 3
+
+	authenticator, err := auth.NewAuthenticator(cfg)
+	assert.NoError(t, err)
+
+	refToken, accToken, err := authenticator.GenerateTokenPairs(*authServiceIDOpt, *usersServiceIDOpt, *usernameOpt)
+	assert.NoError(t, err)
+
+	err = authenticator.DeleteToken(*authServiceIDOpt, *usersServiceIDOpt, *usernameOpt)
+	assert.NoError(t, err)
+
+	t.Run("Test logout refresh token", func(t *testing.T) {
+		t.Parallel()
+		err = authenticator.ValidateToken(refToken, true)
+		assert.Error(t, err)
+	})
+
+	t.Run("Test logout access token", func(t *testing.T) {
+		t.Parallel()
+		err = authenticator.ValidateToken(accToken, false)
+		assert.Error(t, err)
+	})
+}
